f52e8538bc1f2a88f57da23cd267e4fc8fe2f469
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-01-24 02:32:29 +0900

    update

--- Assets/Scripts/Animation/FootIK.cs
+++ Assets/Scripts/Animation/FootIK.cs
@@ -7,6 +7,8 @@ public class FootIK : MonoBehaviour
     [SerializeField] bool _enableHint;
     [SerializeField] float _rayDistance = 1;
     [SerializeField] float _footHeight = 0.2f;
+    [SerializeField] float _footRadius = 0.2f;
+    [SerializeField] LayerMask _layerMask;
     [Header("LeftFoot")]
     [SerializeField] Transform _left;
     [SerializeField, Range(0, 1)] float _leftPositionWeight = 1;
@@ -31,7 +33,6 @@ public class FootIK : MonoBehaviour
 
     private void OnAnimatorIK(int layerIndex)
     {
-        print(_anim.GetFloat("LeftIKWeight"));
         _leftPositionWeight = _anim.GetFloat("LeftIKWeight");
         _leftRotationWeight = _anim.GetFloat("LeftIKWeight");
         _rightPositionWeight = _anim.GetFloat("RightIKWeight");
@@ -49,19 +50,22 @@ public class FootIK : MonoBehaviour
         var toHip = _anim.GetBoneTransform(HumanBodyBones.Hips).position.y - _anim.GetIKPosition(part).y;
         Ray ray = new(_anim.GetIKPosition(part) + Vector3.up * toHip, Vector3.down);
 
-        if (Physics.Raycast(ray, out var hit, _rayDistance))
+        if (Physics.SphereCast(ray, _footRadius, out var hit, _rayDistance, _layerMask))
         {
-            var newIKPos = hit.point;
-            newIKPos.y += _footHeight;
+            if (Physics.Raycast(hit.point + Vector3.up, Vector3.down, out hit, _rayDistance, _layerMask))
+            {
+                var newIKPos = hit.point;
+                newIKPos.y += _footHeight;
 
-            _anim.SetIKPositionWeight(part, positionWeight);
-            _anim.SetIKRotationWeight(part, rotationWeight);
-            _anim.SetIKPosition(part, newIKPos);
+                _anim.SetIKPositionWeight(part, positionWeight);
+                _anim.SetIKRotationWeight(part, rotationWeight);
+                _anim.SetIKPosition(part, newIKPos);
 
-            Debug.DrawRay(hit.point, hit.normal);
-            Debug.DrawRay(hit.point, Vector3.ProjectOnPlane(transform.forward, hit.normal));
+                Debug.DrawRay(hit.point, hit.normal);
+                Debug.DrawRay(hit.point, Vector3.ProjectOnPlane(transform.forward, hit.normal));
 
-            _anim.SetIKRotation(part, Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hit.normal), hit.normal));
+                _anim.SetIKRotation(part, Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, hit.normal), hit.normal));
+            }
         }
     }
 
--- Assets/Scripts/Controlls/PlayerController.cs
+++ Assets/Scripts/Controlls/PlayerController.cs
@@ -4,19 +4,43 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _animationSpeed = 1f;
+    [SerializeField] float _bounce = 0.5f;
+    [SerializeField] float _hipHeight = 1f;
     Vector2 _input;
     Animator _anim;
+    CharacterController _controller;
 
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         _anim.speed = _animationSpeed;
 
-        transform.Rotate(Vector3.up, _input.x);
+        transform.Rotate(Vector3.up, _input.x * 3);
+    }
+
+    void Bounce()
+    {
+        var left = _anim.GetIKPosition(AvatarIKGoal.LeftFoot);
+        var right = _anim.GetIKPosition(AvatarIKGoal.RightFoot);
+        var between = (left + right) / 2;
+        var ray = new Ray(between + Vector3.up * (_hipHeight / 2), Vector3.down);
+
+        if (Physics.Raycast(ray, out var hit, _hipHeight))
+        {
+            var diff = hit.point.y - transform.position.y;
+            print(diff);
+            _controller.transform.position += (diff * _bounce * Time.deltaTime * Vector3.up);
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        Bounce();
     }
 
     void OnMove(InputValue value)
@@ -24,6 +48,13 @@ public class PlayerController : MonoBehaviour
         _input = value.Get<Vector2>();
         print(_input);
 
-        _anim.SetFloat("h", _input.y);
+        _anim.SetFloat("h", _input.x);
+        _anim.SetFloat("v", _input.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * _hipHeight);
     }
 }
